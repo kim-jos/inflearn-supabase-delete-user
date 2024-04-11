f9c57e8e5b9b277edf03e4fbff77b413febb0e53
Joseph Kim <joseph@Josephs-MacBook-Pro.local>
2024-04-11 15:22:43 +0900

    Translated comments from English to Korean

--- delete-user/index.ts
+++ delete-user/index.ts
@@ -4,49 +4,52 @@ import { corsHeaders } from '../_shared/cors.ts';
 
 console.log("Delete user account function");
 
+
+// 아래 github에 있는 코드를 사용했습니다.
+// https://github.com/the-digital-pro/supabase-delete-user/
+
 serve(async (request) => {
 
-  // This is needed if you're planning to invoke your function from a browser.
+  // OPTIONS 요청 메소드 처리를 위한 코드입니다 (브라우저 호환성)
   if (request.method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders })
   }
 
   try {
-    //Create instance of SupabaseClient
+    // Supabase 클라이언트 생성
     const supabaseClient = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
       Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
-      { global: { headers: { Authorization: request.headers.get('Authorization')! } } }
+      { global: { headers: { Authorization: request.headers.get('Authorization')! } } } // 이 부분에서 header에 넣은 사용자 token 사용
     );
 
-    // Create a user object which contains the data we need to identify the user.id
+    // 사용자 ID를 사용하여 사용자 객체 조회
     const {
       data: { user },
     } = await supabaseClient.auth.getUser()
 
-    // Throw an error if there are any issues with identifying the users from the token
+    // 토큰으로부터 사용자 식별에 문제가 있으면 오류 발생
     if (!user) throw new Error('No user found for JWT!');
   
-    // Create supabaseAdmin client which specifically uses the Service Role
-    // Key in order to perform elavated administration actins
+    // 어드민작업을 수행할 수 있도록 서비스 역할 키를 사용하여 supabaseAdmin 클라이언트 생성
     const supabaseAdmin = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
       Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
     )
 
-    // Call the deleteUser method on the supabaseAdmin client and pass the user.id
+    // supabaseAdmin 클라이언트를 사용하고 user.id 전달하여 deleteUser 메소드 호출
     const { data: deletion_data, error: deletion_error } = await supabaseAdmin.auth.admin.deleteUser(user.id)
     
-    // Log deletion error so we can debug. Delete if not required! 
+    // 삭제 오류 로깅, 디버깅을 위함. 필요 없으면 삭제가능!
     console.log(deletion_error);
 
-    // Return a response of the user which has been deleted
+    // 삭제된 사용자의 response
     return new Response('User deleted: ' + JSON.stringify(deletion_data, null, 2), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 200,
     });
   } catch (error) {
-    // Return an error with the error message should it run in to any issues
+    // 문제가 발생한 경우 오류 메시지와 함께 오류 응답 반환
     return new Response(JSON.stringify({ error: error.message }), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 400,
